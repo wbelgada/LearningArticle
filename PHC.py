7afa1157acdfcbd976fa7722d7cb4993c0a7eba4
ielachou <ielachou@ulb.ac.be>
2021-12-31 23:28:02 +0100

    retour ancienne version

--- PHC.py
+++ PHC.py
@@ -4,22 +4,23 @@ import numpy as np
 
 class PHCAgent:
 
-    def __init__(self, n_actions, n_states, delta, alpha, gamma, epsilon, decay, epsilon_min):
+    def __init__(self,n_actions = 2, n_states = 20,delta = 0.02,epsilon = 1,decay = 0.999,epsilon_min = 0.0001):
         self.n_actions = n_actions
         self.n_states = n_states
         self.Q = np.zeros(shape=(n_states, n_actions))
         self.H = (1 / n_actions) * np.ones(shape=(n_states, n_actions))
-        self.alpha = alpha
+        self.alpha = 0.9
         self.delta = delta  # how much we update the policy  if we are winning
-        self.gamma = gamma
+        self.gamma = 0.9
         self.epsilon = epsilon
         self.decay = decay
         self.epsilon_min = epsilon_min
 
 
     def chooseAction(self, s):
+
         if np.random.random() < self.epsilon:
-            action = np.random.choice(self.n_actions)
+            action = np.random.randint(0,self.n_actions)
         else :
             action = np.random.choice(self.n_actions, p = self.H[s])
 
@@ -30,11 +31,11 @@ class PHCAgent:
 
 
         for a in range(self.n_actions):
-            if a == np.argmax(self.Q[s, :]):
-                self.H[s, a] += self.delta  #argamax a => increase probabilty
+            if a == np.argmax(self.Q[s, :]) :
+                self.H[s, a] += self.delta #argamax a => increase probabilty
                 self.H[s, a] = min(1,self.H[s, a])
             else:
-                self.H[s, a] -= self.delta/(self.n_actions-1)  # argamax a => decrease probabilty
+                self.H[s, a] -= self.delta/(self.n_actions-1)  # argamax a => increase probabilty
                 self.H[s, a] = max(0, self.H[s, a])
 
         self.epsilon = max(self.epsilon * self.decay, self.epsilon_min)
--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from matplotlib import pyplot as plt
 def get_payoff(mat , a1, a2):
     return mat[a1][a2],mat[a1][a2]*(-1)
 
-def run_episode(agents,mat, train ):
+def run_episode(agents,mat, train):
     a1 = agents[0].chooseAction(0)
     a2 = agents[1].chooseAction(0)
-    #a2 = np.random.randint(0,1)
+
     r1,r2 = get_payoff(mat, a1, a2)
 
     if train :
-        agents[0].update(0, a1, r1, 0)
-        agents[1].update(0, a2, r2, 0)
+        agents[0].update(0, r1, a1, 0)
+        agents[1].setExperience(0,a2,r2,None, 0)
+        agents[1].update(0)
 
     return a1,a2
 
-def train(num_episodes: int, evaluate_every: int, num_evaluation_episodes: int) :
+def train(num_episodes: int, evaluate_every: int, num_evaluation_episodes: int, epsilon: int,  decay: int, epsilon_min: int) :
     """
         Training loop.
-
         :param env: The gym environment.
         :param gamma: The discount factor.
         :param num_episodes: Number of episodes to train.
@@ -36,22 +36,10 @@ def train(num_episodes: int, evaluate_every: int, num_evaluation_episodes: int) 
         """
 
     returns = np.zeros(shape=((num_episodes//evaluate_every),num_evaluation_episodes,2))
-
-    mat = np.array([[1, -1],
-                    [-1, 1]])
-
-    n_states = 1
-    n_action = len(mat[0])
-
-    delta_win = 0.001
-    delta_lose = 0.1
-
-    alpha = 0.1
-    gamma = 0.999
-
-
-    agents = [PHCAgent(n_action, n_states,delta_win,alpha,gamma,epsilon,decay,epsilon_min),WoLFPHCAgent(n_action,n_states,delta_win,delta_lose)]
-
+    agents = [PHCAgent(2, 1, 0.05,epsilon,decay,epsilon_min),WoLFPHCAgent(2,1,0.1,0.999,0.01,0.1,0.0)]
+    mat = np.array([[1,-1],
+                    [-1,1]])
+    current_eval = 0
     probs1 = []
     probs2 = []
     for i in range(num_episodes):
@@ -59,11 +47,22 @@ def train(num_episodes: int, evaluate_every: int, num_evaluation_episodes: int) 
 
         if ((i+1) % evaluate_every == 0) :
             print("Training for", i+1 , "/", num_episodes)
-            print("Probability for this evaluation  is for agent 1 :", agents[0].H[0][0])
-            print("Probability for this evaluation  is for agent 2 :", agents[1].H[0][0])
+            for episode in range(num_evaluation_episodes):
+                actions = run_episode(agents,mat,False)
+                returns[current_eval][episode] = actions
+            count1 = 0
+            count2 = 0
+            for elem in returns[current_eval]:
+                if elem[0] == 0 :
+                    count1 += 1
+                if elem[1] == 0:
+                    count2 += 1
+            current_eval += 1
+            print("Probability for this evaluation  is for agent 1 :", count1 /num_evaluation_episodes)
+            print("Probability for this evaluation  is for agent 2 :", count2 / num_evaluation_episodes)
 
-            probs1.append(agents[0].H[0][0])
-            probs2.append(agents[1].H[0][0])
+            probs1.append(count1/num_evaluation_episodes)
+            probs2.append(count2 / num_evaluation_episodes)
 
     return agents,returns,probs1,probs2
 
@@ -72,24 +71,18 @@ def train(num_episodes: int, evaluate_every: int, num_evaluation_episodes: int) 
 
 if __name__ == "__main__" :
 
-    num_episodes = 1000000
+    num_episodes = 100000
     evaluate_every = 10000
-    num_evaluation_episodes = 32
+    num_evaluation_episodes = 100
     epsilon = 1
-    decay = 0.9999999
-    epsilon_min = 0.000000001
-    nb_runs = 1
+    decay = 0.999
+    epsilon_min = 0.00001
 
+    agents,returns,probs1,probs2 = train(num_episodes,evaluate_every,num_evaluation_episodes,epsilon,decay,epsilon_min)
 
-    tot_probs1 = np.zeros(shape=(num_episodes//evaluate_every))
-    tot_probs2 = np.zeros(shape=(num_episodes//evaluate_every))
-    for i in range(nb_runs) :
-        agents, returns, probs1, probs2 = train(num_episodes,evaluate_every,num_evaluation_episodes)
-        tot_probs1 += probs1
-        tot_probs2 += probs2
+    print(probs1)
+    print(probs2)
 
-    tot_probs1 /= nb_runs
-    tot_probs2 /= nb_runs
 
     fig, ax = plt.subplots()
 
@@ -97,8 +90,8 @@ if __name__ == "__main__" :
     x_train_pos = np.arange(0, num_episodes, evaluate_every)
 
 
-    plt.plot(x_train_pos, probs1, label="PHC")
+    plt.plot(x_train_pos, probs1, label="Training average")
 
-    #plt.plot(x_train_pos, probs2, label="WoLF-PHC")
-    plt.legend()
+    plt.plot(x_train_pos, probs2, label="Training average")
+
     plt.show()
